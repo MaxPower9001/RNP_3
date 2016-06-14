2d6f0b7850dbcab7dc7241d9e89c5a210b3bf9b0
moin <moin@moin.com>
2016-06-14 19:20:12 +0200

    Hatte ich commmited?

--- abgabe_prak4/run.sh
+++ abgabe_prak4/run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-if [[ $1 == "sctp" ]] ; then java -jar rnp_sctp.jar ; fi
 if [[ $1 == "tcp" ]] ; then java -jar rnp_tcp.jar ; fi
+if [[ $1 == "sctp" ]] ; then 
+  echo -n "Hearbeat Intervall: "
+  sysctl net.sctp.hb_interval
+  [[ $2 ]] && ( MILLIS=$2; sudo sysctl net.sctp.hb_interval=$MILLIS )
+  java -jar rnp_sctp.jar 
+fi
